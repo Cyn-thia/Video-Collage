018a065fbbe6c60aaccff075ea30d2d7618a2574
Cynthia Nguyen <cynthia@ool-ad0201be.dyn.optonline.net>
2018-11-27 11:26:09 -0500

    added back button to camera screen

--- Camera.js
+++ Camera.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, Button } from 'react-native'
+import { View, TouchableOpacity, Text, Button, StyleSheet } from 'react-native'
 import { withNavigation } from 'react-navigation';
 import { Camera, Permissions, FileSystem } from 'expo';
 import * as Expo from "expo";
@@ -22,38 +22,84 @@ var storage = firebase.storage();
 var storageRef = storage.ref();
 
 export default class Cam extends React.Component {
-  state = {
-    hasCameraPermission: null,
-    hasAudioPermission: null,
-    type: Camera.Constants.Type.back,
-  };
+    state = {
+      hasCameraPermission: null,
+      hasAudioPermission: null,
+      type: Camera.Constants.Type.front,
+      record: 'Record',
+    };
 
-  async componentDidMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA, Permissions.AUDIO_RECORDING);
-    this.setState({
-      hasCameraPermission: status === 'granted',
-      hasAudioPermission: status === 'granted'
-    });
-  }
-
-  recVid = () => {
-    if (this.camera) {
-      this.camera.recordAsync({ maxDuration: 100000, mute: false })
-      .then(response => { this.uploadVidAsync(response) })
-      // this.props.currentVid = response.uri
+    async componentDidMount() {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA, Permissions.AUDIO_RECORDING);
+      this.setState({
+        hasCameraPermission: status === 'granted',
+        hasAudioPermission: status === 'granted'
+      });
     }
-  }
 
-  uploadVidAsync = async (photo) => {
-    const response = await `${photo.uri}`;
-    console.log(response)
-    const blob = await new Blob([response], { type: `video/mp4` });
-    console.log(blob)
-    var vidRef = await storageRef.child(photo.uri);
-    const snapshot = await vidRef.put(blob);
-    console.log('sanity check')
-    return snapshot.downloadURL;
-  }
+    recVid = () => {
+      if (this.camera) {
+        this.camera.recordAsync({ maxDuration: 100000, mute: false })
+          .then(response => { this.uploadVidAsync(response) })
+        // this.props.currentVid = response.uri
+      }
+    }
+
+    uploadVidAsync = async (photo) => {
+      const response = await `${photo.uri}`;
+      console.log(response)
+      const blob = await new Blob([response], { type: `video/mp4` });
+      console.log(blob)
+      var vidRef = await storageRef.child(photo.uri);
+      const snapshot = await vidRef.put(blob);
+      console.log('sanity check')
+      return snapshot.downloadURL;
+    }
+
+    renderTopBar = () =>
+      <View style={styles.topBar}>
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => { this.props.navigation.navigate('Collage') }}>
+          <Text
+            style={{ fontSize: 18, marginTop: 0, color: 'white' }}>
+            {' '}Back{' '}
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+    renderBottomBar = () =>
+      <View
+        style={styles.bottomBar}>
+        <TouchableOpacity
+          style={styles.flipButton}
+          onPress={() => {
+            this.setState({
+              type: this.state.type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back,
+            });
+          }}>
+          <Text
+            style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+            {' '}Flip{' '}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            flex: .5,
+            alignSelf: 'flex-end',
+            alignItems: 'center',
+          }}
+          onPress={ this.recVid }
+
+          >
+          <Text
+            style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+            {this.state.record}
+          </Text>
+        </TouchableOpacity>
+      </View>
 
 
   render() {;
@@ -66,52 +112,40 @@ export default class Cam extends React.Component {
       return (
         <View style={{ flex: 1 }}>
           <Camera style={{ flex: 1 }} type={this.state.type}
-            ref={ref => { this.camera = ref; }}
-          >
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}>
-
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={() => {
-                  this.setState({
-                    type: this.state.type === Camera.Constants.Type.back
-                      ? Camera.Constants.Type.front
-                      : Camera.Constants.Type.back,
-                  });
-                }}>
-                <Text
-                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                  {' '}Flip{' '}
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={{
-                  flex: 1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={ this.recVid
-                }>
-                <Text
-                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                  {' '}Record{' '}
-                </Text>
-              </TouchableOpacity>
-            </View>
-
+            ref={ref => { this.camera = ref; }}>
+            {this.renderTopBar()}
+            {this.renderBottomBar()}
           </Camera>
-
         </View>
       );
     }
   }
 }
+
+const styles = StyleSheet.create({
+  topBar: {
+    flex: 0.1,
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+  },
+  backButton: {
+    flex: 0.2,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    fontSize: 18,
+    marginTop: 0,
+    color: 'white'
+  },
+  bottomBar: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+  },
+  flipButton: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  }
+  })
